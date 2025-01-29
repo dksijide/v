7f8eb55b61ba7d54344d6a0190a2b71ae249cfd6
dksijide <jehudizon40@gmail.com>
2025-01-30 00:33:34 +0800

    Update V3schan.lua

--- V3schan.lua
+++ V3schan.lua
@@ -1,5 +1,15 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+local Window = Fluent:CreateWindow({
+    Title = "Trash(ごみ) " .. "0.0.2",
+    SubTitle = "- puuror",
+    TabWidth = 80,
+    Size = UDim2.fromOffset(450, 350),
+    Acrylic = false,
+    Theme = "Rose",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
 local v = {"V3schan", "Woodtoolsc"}
 local v2 = false
 
