d808ad8c9982765dcac595da673a202bbf11038f
dksijide <jehudizon40@gmail.com>
2025-01-30 01:00:29 +0800

    Update testfile.lua

--- testfile.lua
+++ testfile.lua
@@ -1,1 +1,61 @@
-print(hi.Name)
+local v = {"V3schan", "Woodtoolsc"}
+local v2 = false
+
+for _, username in pairs(v) do
+    if game.Players.LocalPlayer.Name == username then
+        v2 = true
+        break
+    end
+end
+
+if v2 then
+	Fluent:Notify({
+        Title = "Welcome me",
+        Content = "Welcome " .. game.Players.LocalPlayer.DisplayName",
+        Duration = 5 
+    })
+		local Tabs = {
+				Personal = Window:AddTab({ Title = "Personal", Icon = "archive" }),
+			}
+
+		Tabs.Personal:AddButton({
+        Title = "0 WinsStreak",
+        Description = "Making an all coins account 0 Wins",
+        Callback = function()
+
+			Fluent:Notify({
+					Title = "Hello",
+					Content = "Please wait for 2 players left until u die",
+					Duration = 5 
+				})
+
+					local hi = game.Workspace.plrsLeft
+					local player = game.Players.LocalPlayer
+
+					if not player.Character or not player.Character:FindFirstChild("Humanoid") then
+						player.CharacterAdded:Wait()
+					end
+
+					local character = player.Character
+					local humanoid = character:WaitForChild("Humanoid")
+
+					local playerDead = false
+					humanoid.Died:Connect(function()
+						playerDead = true
+					end)
+
+					while not playerDead do
+						if hi.Value == 2 then
+							humanoid.Health = 0
+						end
+						wait(0.1)
+					end
+				end
+			})
+		else
+    Fluent:Notify({
+        Title = "Can't be open",
+        Content = "Sorry only the owner can open this...",
+        Duration = 5 
+    })
+end
