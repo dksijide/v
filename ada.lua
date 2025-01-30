9dbf4491961bdbea2239e06b19679e1c4653c663
dksijide <jehudizon40@gmail.com>
2025-01-30 10:19:00 +0800

    Create ada.lua
    adda

---
+++ ada.lua
@@ -0,0 +1,9 @@
+local Window = Fluent:CreateWindow({
+    Title = "Trash(ごみ) " .. "0.0.2",
+    SubTitle = "- puuror",
+    TabWidth = 80,
+    Size = UDim2.fromOffset(450, 350),
+    Acrylic = false,
+    Theme = "Rose",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
