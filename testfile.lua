d1d53d48ad3a86b9e63b94a79aa9b0c2191db6d5
dksijide <jehudizon40@gmail.com>
2025-01-30 10:18:13 +0800

    Update testfile.lua
    adada

--- testfile.lua
+++ testfile.lua
@@ -1,1 +1,9 @@
-print(hi)
+local Window = Fluent:CreateWindow({
+    Title = "Trash(ごみ) " .. "0.0.2",
+    SubTitle = "- puuror",
+    TabWidth = 80,
+    Size = UDim2.fromOffset(450, 350),
+    Acrylic = false,
+    Theme = "Rose",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
