7f08dd884f5045aa2d3abbd9bd4683e98ea66f17
dksijide <jehudizon40@gmail.com>
2025-01-30 10:57:17 +0800

    Update ckdaa.lua

--- cdaa.lua
+++ cdaa.lua
@@ -8,27 +8,7 @@
         Description = "Making an all coins account 0 Wins",
         Callback = function()
 
-					local hi = game.Workspace.plrsLeft
-					local player = game.Players.LocalPlayer
-
-					if not player.Character or not player.Character:FindFirstChild("Humanoid") then
-						player.CharacterAdded:Wait()
-					end
-
-					local character = player.Character
-					local humanoid = character:WaitForChild("Humanoid")
-
-					local playerDead = false
-					humanoid.Died:Connect(function()
-						playerDead = true
-					end)
-
-					while not playerDead do
-						if hi.Value == 2 then
-							humanoid.Health = 0
-						end
-						wait(0.1)
-					end
+	
 				end
 			})
 
