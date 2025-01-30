2b0645cf2c60da7d469cfd7312751155ffb821b5
dksijide <jehudizon40@gmail.com>
2025-01-30 10:29:55 +0800

    Create a.lua

---
+++ a.lua
@@ -0,0 +1,7 @@
+     Tabs.Skids:AddButton({
+        Title = "Inf-yield",
+        Description = "Most used script in skywars even in other games!",
+        Callback = function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end
+    })
