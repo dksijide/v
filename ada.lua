62f3b01a3b98fb8c1260c3a938a23b02c9cfc31e
dksijide <jehudizon40@gmail.com>
2025-01-30 10:29:23 +0800

    Delete ada.lua

--- ada.lua
+++
@@ -1,9 +0,0 @@
-local Window = Fluent:CreateWindow({
-    Title = "Trash(ごみ) " .. "0.0.2",
-    SubTitle = "- puuror",
-    TabWidth = 80,
-    Size = UDim2.fromOffset(450, 350),
-    Acrylic = false,
-    Theme = "Rose",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
