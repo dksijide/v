2b798d9b3a2d803ded40638bf3762c93cf5fac3a
dksijide <jehudizon40@gmail.com>
2025-01-30 20:30:18 +0800

    Delete a.lua

--- a.lua
+++
@@ -1,7 +0,0 @@
-     Tabs.Skids:AddButton({
-        Title = "Inf-yield",
-        Description = "Most used script in skywars even in other games!",
-        Callback = function()
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-        end
-    })
